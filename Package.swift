2b8884546444e5000c7658eac741fdadf3fb78dd
0xLeif <leif.algo@pm.me>
2025-12-02 17:04:58 -0700

    Add swift-docc-plugin for documentation generation

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
     products: [
         .library(name: "Color", targets: ["Color"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.3")
+    ],
     targets: [
         .target(name: "Color"),
         .testTarget(
